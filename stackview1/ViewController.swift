a0a962f902141972ce96ccb872c10431b4cec576
John Greathouse <johngreathouse@johns-imac.local>
2016-07-27 17:21:47 -0500

    Initial Commit

---
+++ stackview1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stackview1
+//
+//  Created by John Greathouse on 7/27/16.
+//  Copyright © 2016 John Greathouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
